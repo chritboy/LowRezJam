304e754b1316fafea4cf23a885f7147e4afd9608
Chris <chris.j.laforce@gmail.com>
2023-08-02 17:30:59 -0400

    walking and attack melee animations completed

--- Scenes/Characters/player.gd
+++ Scenes/Characters/player.gd
@@ -1,15 +1,39 @@
 extends CharacterBody2D
-@onready var animated_sprite = $Sprite2D
+
+enum {
+	MOVE,
+	ATTACK,
+	DODGE
+}
+
+@onready var animated_sprite = $Sprite
 @onready var anim = $AnimationPlayer
 @onready var anim_tree = $AnimationTree
+@onready var anim_state = anim_tree.get("parameters/playback")
+
 @export var speed : int = 50
 @export var friction : float = 0.15
 @export var acceleration : int = 40
 
+var current_state = MOVE
 var is_moving = false
 
+
+func _ready():
+	anim_tree.active = true
+	
 func _physics_process(_delta):
-	movement()
+	match current_state:
+		MOVE:
+			movement()
+			if Input.is_action_just_pressed("Attack"):
+				current_state = ATTACK
+	
+		ATTACK:
+			attack()
+		
+		DODGE:
+			pass
 	
 	
 func movement():
@@ -20,10 +44,18 @@ func movement():
 	velocity = input_vector * speed
 	
 	if velocity == Vector2.ZERO:
-		anim_tree.get("parameters/playback").travel("Idle")
+		anim_state.travel("Idle")
 	else:
-		anim_tree.get("parameters/playback").travel("Walk")
 		anim_tree.set("parameters/Idle/blend_position", velocity)
 		anim_tree.set("parameters/Walk/blend_position", velocity)
+		anim_tree.set("parameters/MeleeAtk/blend_position", velocity)
+		anim_state.travel("Walk")
 	
 	move_and_slide()
+
+func attack():
+	velocity = Vector2.ZERO
+	anim_state.travel("MeleeAtk")
+
+func attack_finished():
+	current_state = MOVE
