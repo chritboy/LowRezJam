3f6c9f6421b96df1f650d113cfe61d22e5d99964
Chris <chris.j.laforce@gmail.com>
2023-08-11 16:26:56 -0400

    added player death, respawn, door capabilities, and title menu

--- Scripts/flies.gd
+++ Scripts/flies.gd
@@ -18,17 +18,19 @@ var player = null
 
 
 func _ready():
+	Events.player_died.connect(self.on_player_died)
 	player = get_tree().get_first_node_in_group("Player")
 	
 func _physics_process(delta):
 	anim_player.play("V1_Walk")
-	if player.is_dodging == true:
+	if Player.is_dodging == true:
 		self.set_collision_mask_value(1, false)
 	else:
 		self.set_collision_mask_value(1, true)
 	
-	var dir = global_position.direction_to(player.global_position)
-	velocity = velocity.move_toward(dir * chase_speed, acceleration * delta)
+	if player != null:
+		var dir = global_position.direction_to(player.global_position)
+		velocity = velocity.move_toward(dir * chase_speed, acceleration * delta)
 		
 	if soft_coll.is_colliding():
 		velocity += soft_coll.get_push_vector() * delta * 200
@@ -43,3 +45,6 @@ func knockback(enemy_velocity):
 	var knockback_dir = (enemy_velocity - velocity).normalized() * knockback_power
 	velocity = knockback_dir
 	move_and_slide()
+
+func on_player_died():
+	queue_free()
---
+++ Scripts/stage_manager.gd
@@ -0,0 +1,24 @@
+extends CanvasLayer
+
+const MainHall = preload("res://Scenes/Screens/MainHall.tscn")
+const WestHall = preload("res://Scenes/Screens/WestHall.tscn")
+
+
+func _ready():
+	self.hide()
+
+func change_stage(stage_path, x, y):
+	self.show()
+	$AnimationPlayer.play("TransOut")
+	get_tree().paused = true
+	await $AnimationPlayer.animation_finished
+	
+	var stage = stage_path.instantiate()
+	get_tree().get_root().get_child(3).free()
+	get_tree().get_root().add_child(stage)
+	stage.get_node("Player").position = Vector2(x, y)
+	
+	$AnimationPlayer.play("TransIn")
+	await $AnimationPlayer.animation_finished
+	self.hide()
+	get_tree().paused = false
